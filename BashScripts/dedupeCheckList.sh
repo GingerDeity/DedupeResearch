d89de633beb1697459e1820a96a7dab2971a62c7
GingerDeity <131397081+GingerDeity@users.noreply.github.com>
2025-03-13 13:33:06 -0400

    Added comments to dedupeCheckList.sh

--- BashScripts/dedupeCheckList.sh
+++ BashScripts/dedupeCheckList.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-#$1 is window size, $2 will be base file that all others will compare against, $3 will be directory to do DedupeCheck on
+# This code outputs static deduplication results and takes in a window size, a 'base' file, 
+# and a directory of files to compare against the base. So, if we run this on a directory 
+# named 'example' containing files A, B, C, using the command: 
+# "bash dedupeCheckList.sh 4096 A example/", this outputs results for: 
+# [(A), (A, B), (A, C)]
+# $1 is window size, $2 will be base file that all others will compare against, $3 will be directory to do DedupeCheck on
 
 echo "SCANNING $2"
 java /mnt/data/atreyu/ParsingTools/DedupeCheck.java $1 "$2"
