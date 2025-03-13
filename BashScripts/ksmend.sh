83514e30c5f73386f439c5c218581b3fd2fcabe2
GingerDeity <131397081+GingerDeity@users.noreply.github.com>
2025-03-13 13:14:44 -0400

    Added comments to ksmend.sh

--- BashScripts/ksmend.sh
+++ BashScripts/ksmend.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Tells KSM to finish and unmerge all pages currently merged, but leave mergeable areas registered for next run
+# Optionally, you can add PIDs that you want to kill. These PIDs are meant to be processes that were being used by KSM
 
 echo 2 | sudo tee /sys/kernel/mm/ksm/run
 for pid in "$@"
