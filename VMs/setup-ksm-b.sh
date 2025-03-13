d74720c06532e5c1f94bda6bf6000d0f4aeaaaba
GingerDeity <131397081+GingerDeity@users.noreply.github.com>
2025-03-13 14:15:14 -0400

    Add comments to setup-ksm-b.sh

--- VMs/setup-ksm-b.sh
+++ VMs/setup-ksm-b.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# This code should be run from the perspective of the host machine. 
+# This sets up one of 2 level-1 VMs, which I call KSM-B1. You'll have to 
+# ssh into it using the connect-ksm-b.sh script!
+
 EXE=qemu-system-x86_64
 IMG=QemuKVM/IMGs/ubuntu18_250G_2.img
 #CDROM=QemuKVM/CentOS-7-x86_64-Minimal-2009.iso
