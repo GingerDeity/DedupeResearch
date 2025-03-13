0367c14aeda4191831a0d8e59fcc770f4f0a3052
GingerDeity <131397081+GingerDeity@users.noreply.github.com>
2025-03-13 13:18:05 -0400

    Added comments to ksminit.sh

--- BashScripts/ksminit.sh
+++ BashScripts/ksminit.sh
@@ -4,5 +4,5 @@
 # in the ksm_zero_pages field), and ensures KSM is in the 'terminated' state
 
 echo 500000 | sudo tee /sys/kernel/mm/ksm/pages_to_scan
-echo 1 | sudo tee /sys/kernel/mm/ksm/use_zero_pages
+echo 1 | sudo tee /sys/kernel/mm/ksm/use_zero_pages 
 echo 2 | sudo tee /sys/kernel/mm/ksm/run
