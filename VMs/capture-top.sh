a1259d5273e4f5c50871337066beac8b057b5e74
GingerDeity <131397081+GingerDeity@users.noreply.github.com>
2025-03-20 14:35:17 -0400

    Update capture-top.sh

--- VMs/capture-top.sh
+++ VMs/capture-top.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-# A simple shell script that outputs top to a text file while a process runs
-# This is intended to be used in LVL-2 VMs, able to capture process RESs, which
-# are useful for deduplication measurements. The test file will show the final RES
-# size before the process ends. Change the text file name as needed
+# A shell script that outputs a filtered top in given time increments to 
+# a text file while a given process runs. This is intended to be used in 
+# LVL-2 VMs for capturing process RESs of interest, which are useful for 
+# deduplication measurements. The text file will show the final output of top 
+# before the process ends. Change the text file name as needed.
 # 1st arg = PID of the process
 
 PID=$1
+out="LVL2-top.txt"
 while kill -0 "$PID" 2>/dev/null; do
-	top -b -n 1 > LVL2-top.txt
-	sleep 1
+  	top -b -n 1 | awk -v pid="$PID" '
+   		NR <= 7|| $1 == pid' >> "$out"
+     	sleep 1
 done
