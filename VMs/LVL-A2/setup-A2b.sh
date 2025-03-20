f6327ded1b0f6809735d0987f3e372b49b343136
GingerDeity <131397081+GingerDeity@users.noreply.github.com>
2025-03-20 12:49:19 -0400

    Update and rename setup-2b.sh to setup-A2b.sh

--- VMs/LVL-A2/setup-A2b.sh
+++ VMs/LVL-A2/setup-A2b.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+# Sets up KSM-A2b, alter IMG, NUMCPUS, and MEMSIZE as needed
 EXE=qemu-system-x86_64
 IMG=QemuKVM/ubuntu20_80G_2.img
 #CDROM=QemuKVM/CentOS-7-x86_64-Minimal-2009.iso
 NUMCPUS=8
 MEMSIZE=15G
 #MEMSIZE=32G,slots=2,maxmem=42G
-MONITOR='telnet:127.0.0.1:1238,server,nowait'
-NET='user,hostfwd=tcp::10028-:22'
+MONITOR='telnet:127.0.0.1:1236,server,nowait'
+NET='user,hostfwd=tcp::10025-:22'
 LOG=./running_ppcm.log
 #HUGE_PAGES=/huge
 
