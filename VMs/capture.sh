5df8d66dd2bf6497dc5ef784f5f912a1f78193ff
GingerDeity <131397081+GingerDeity@users.noreply.github.com>
2025-03-20 14:39:53 -0400

    Update capture.sh

--- VMs/capture.sh
+++ VMs/capture.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-PID2a=$1
-PID2b=$2
+# To be used in LVL-1 VM, KSM-B. This ssh's into both LVL-2 VMs to check if
+# given PIDs are still running and, if so, outputs LVL-1 top info and KSM info 
+# to output files. Assumes KSM is already initialized and running. Change the
+# output file names and pathnames as necessary!
+
+PID_B2a=$1
+PID_B2b=$2
+KSMout="LVL-B1-ksm.txt"
+TOPout="LVL-B1-top.txt"
 echo "KSM iterating"
-while sudo sshpass -p "admin" ssh -o StrictHostKeyChecking=no josh@localhost -p 10027 "kill -0 $PID2a > /dev/null" && \
-	sudo sshpass -p "admin" ssh -o StrictHostKeyChecking=no josh@localhost -p 10028 "kill -0 $PID2b > /dev/null"; do
-	top -b -n 1 > LVL1-top.txt
-	./BashScripts/ksmls.sh > LVL1-ksm.txt
+while sudo sshpass -p "admin" ssh -o StrictHostKeyChecking=no josh@localhost -p 10027 "kill -0 $PID_B2a > /dev/null" && \
+	sudo sshpass -p "admin" ssh -o StrictHostKeyChecking=no josh@localhost -p 10028 "kill -0 $PID_B2b > /dev/null"; do
+	top -b -n 1 > "$TOPout"
+	./BashScripts/ksmls.sh > "$KSMout"
 	sleep 2
 done
 echo "KSM has ended"
