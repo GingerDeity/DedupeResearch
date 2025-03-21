e932817e9ea4c317f510e5467f9a565dbf3ec148
atreyum <atreyum@vt.edu>
2025-03-21 18:04:56 -0400

    added comments to all test files

---
+++ Testing/TestKSM/readme.txt
@@ -0,0 +1,2 @@
+Contains code and it's last compiled executable that will create duplicate
+pages for you to test KSM on.
--- Testing/TestKSM/testKSM.c
+++ Testing/TestKSM/testKSM.c
@@ -1,31 +1,43 @@
+/*
+ * Code that simply allocates a certain number of pages all sharing the 
+ * same data. If KSM is working correctly, it'll pick up on the pages
+ * allocated.
+ *
+ * Compiled with" gcc -o0 -o testKSM testKSM.c
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/mman.h>
 
 int main() {
-	size_t size = 4096 * 5; //each call to posix_memalign() allocates five pages
-	
+	size_t size = 4096 * 5; // each call to posix_memalign() allocates five pages
 	char* memory1;
 	char* memory2;
+	
+	// Aligns memory1 to a 4kB address in memory
 	if (posix_memalign((void**)&memory1, 4096, size) != 0) {
 		printf("memalign mem1 didn't work\n");
 		return 1;
 	}
 
+	// Aligns memory2 to a 4kB address in memory
 	if (posix_memalign((void**)&memory2, 4096, size) != 0) {
 		printf("memalign mem2 didn't work\n");
 		return 1;
 	}
 
+	// Sets the data in strings to repeat 'A'
 	memset(memory1, 'A', size);
 	memset(memory2, 'A', size);
 
+	// Tells kernel that the memory1 region is KSM-mergeable
 	if (madvise(memory1, size, MADV_MERGEABLE) != 0) {
 		printf("madvise mem1 didn't work\n");
 		return 1;
 	}
 
+	// Tells kernel that the memory2 region is KSM-mergeable
 	if (madvise(memory2, size, MADV_MERGEABLE) != 0) {
 		printf("madvise mem2 didn't work\n");
 		return 1;
