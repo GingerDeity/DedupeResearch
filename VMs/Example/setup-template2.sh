9f2d48012d9b65bdbc8fdda4cca2dc2e9780f391
GingerDeity <atreyumclewin@gmail.com>
2025-06-13 10:32:11 -0400

    added new scripts

---
+++ VMs/Example/setup-template2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+EXE=qemu-system-x86_64
+IMG=QemuKVM/ubuntu20_template2.img
+#CDROM=QemuKVM/CentOS-7-x86_64-Minimal-2009.iso
+NUMCPUS=8
+MEMSIZE=8G
+#MEMSIZE=32G,slots=2,maxmem=42G
+MONITOR='telnet:127.0.0.1:1237,server,nowait'
+NET='user,hostfwd=tcp::10027-:22'
+LOG=./running_ppcm.log
+#HUGE_PAGES=/huge
+
+date >> ${LOG}
+# hostname >> ${LOG}
+${EXE} \
+    -hda ${IMG} \
+    -m ${MEMSIZE} \
+    -smp ${NUMCPUS} \
+    -enable-kvm \
+    -monitor ${MONITOR} \
+    -curses \
+    -boot menu=on \
+    -net ${NET} \
+    -net nic \
+    -cpu host 
